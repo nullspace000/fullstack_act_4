ea45c315852c5507e440e0118768851b11b49255
nullspace <nullspace000@protonmail.com>
2026-02-11 21:31:51 -0600

    .

--- front/script.js
+++ front/script.js
@@ -1,6 +1,3 @@
-// ===============================
-// 3. Crear y agregar elementos dinámicamente
-// ===============================
 
 const productList = document.getElementById("product_list");
 const btnAgregarItem = document.getElementById("btnAgregarItem");
@@ -10,13 +7,14 @@ let contadorItems = 2;
 
 btnAgregarItem.addEventListener("click", function () {
   contadorItems++;
+  console.log(contadorItems);
 
   // document.createElement: crea un nuevo nodo del tipo indicado. [web:29][web:80]
-  const nuevoLi = document.createElement("li");
+  const nuevoProducto = document.createElement("li");
 
   // Asignar texto al nuevo elemento
-  nuevoLi.textContent = "Elemento agregado " + contadorItems;
+  nuevoProducto.textContent = "Bocina" + contadorItems;
 
   // appendChild: agrega el nuevo nodo como hijo del elemento padre. [web:29]
-  listaDemo.appendChild(nuevoLi);
+  productList.appendChild(nuevoProducto);
 });
